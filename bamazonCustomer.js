b283121caaf5e870ad7cbb2a16dae0e72cb672ed
herbiemolina <herbiemolina@gmail.com>
2019-04-16 09:59:18 -0400

    insuficiant quantity function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,27 +17,73 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_db"
   });
-  
+  //checks connection to database
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
   });
-
+  
+//displays all the products in db
   function readProducts() {
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM Products", function(err, res) {
       if (err) throw err;
       // Log all results of the SELECT statement
       console.log(columnify(res));
+    
       youbuyNow();
     });
+    
 };
 
+// var purchaseNum = connection.query("SELECT stock_quantity FROM bamazon_db.products WHERE item_id = " + answer.whatyoubuy, function (err, res) {
+    
+
+//prompts the user what do they want to do?
 function youbuyNow() {
-    inquirer.prompt({
-        name: "action",
-        type: "number",
-        message: "What would product (ID) would you like to buy?"
+    inquirer.prompt([
+        {
+            name: "whatyoubuy",
+            type: "number",
+            message: "What product (ID) would you like to buy?"
+        },
+
+        {
+            name: "howmany",
+            type: "number",
+            message: "How many would you like to buy?"
+        }
+    
+]).then(function(answer) {
+    connection.query("SELECT stock_quantity FROM bamazon_db.products WHERE ?",
+    {
+        item_id: answer.whatyoubuy
+    },
+    function (err, res){
+        if (answer.howmany > res[0].stock_quantity) {
+            console.log("Insuficiant Quantity")
+            console.log("There are only " + res[0].stock_quantity + " in stock!")
+            inquirer.prompt([
+                {
+                name: "retry",
+                type:"list",
+                message: "Do you want to try another quantity?",
+                choices: 
+                [
+                "Yes, I'd like to try another quantity.",
+                "No, I'd like to exit."
+                ]
+                }
+
+            ])
+        }
+        // else{
+            // updateStock();
+        // }
     })
+    
+    })
+
+    
 }
